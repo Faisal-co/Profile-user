bd5b2b4ce5d468268c6cb29014cfcc660ecdaab9
Faisal <softtechrana05@gmail.com>
2026-02-08 14:06:01 +0500

    update-pageprofilecode

--- pageprofile.php
+++ pageprofile.php
@@ -1,7 +1,16 @@
 <?php
 include 'connection.php';
-session_start();
-if(!isset($_SESSION['user_id'])){
+session_start();?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Hi, ".$user['username']; ?></title>
+</head>
+<body style="background-color: green;">
+<?php
+    if(!isset($_SESSION['user_id'])){
     header("Location: login-userprofile.php");
 }
 $id = $_SESSION['user_id'];
@@ -10,7 +19,11 @@ $sql = "SELECT * FROM profil WHERE id='$id'";
 $result = mysqli_query($conn,$sql);
 $user = mysqli_fetch_assoc($result);
 ?>
-<img src="uploads/<?php echo $user['imag']; ?>" width="120"><br><br>
-<h1> <?php echo "Hi, ".$user['username']; ?> </h1>
-<h1> <?php echo $user['email']; ?> </h1>
+<div style="margin-left: 500px; margin-top: 100px;"> <img src="uploads/<?php echo $user['imag']; ?>" width="120"><br><br> </div>
+<h1 style="margin-left: 500px; color: white;"> <?php echo "Hi, ".$user['username']; ?> </h1>
+<h1 style = "margin-left: 460px; color: white;"> <?php echo $user['email']; ?> </h1>
+</body>
+</html>
+
+
 
